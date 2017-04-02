3700bfd2ec2ac4d818c9aceb265c4775398202c7
Luke Granger-Brown <git@lukegb.com>
2017-04-02 14:18:22 +0100

    Add support for more types to configtable

--- ngdp/configtable/configtable.go
+++ ngdp/configtable/configtable.go
@@ -2,9 +2,11 @@ package configtable
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -18,6 +20,7 @@ const (
 type column struct {
 	name    string
 	colType string
+	byteLen int
 }
 
 // A Decoder reads a Blizzard config table from an input stream.
@@ -63,14 +66,26 @@ func (d *Decoder) readHeader() error {
 			return d.err
 		}
 
-		if bits[1] != "STRING:0" {
-			d.err = fmt.Errorf("configtable: unsupported type %q; only strings supported!")
+		blizzType := strings.Split(strings.ToLower(bits[1]), ":")
+		if len(blizzType) != 2 {
+			d.err = fmt.Errorf("configtable: expected type to be TYPENAME:BYTELEN; got %q", bits[1])
+			return d.err
+		}
+		byteLen, err := strconv.Atoi(blizzType[1])
+		if err != nil {
+			d.err = fmt.Errorf("configtable: expected type to be TYPENAME:BYTELEN; got %q: %v", bits[1], err)
+			return d.err
+		}
+
+		if blizzType[0] != "string" && blizzType[0] != "hex" && blizzType[0] != "dec" {
+			d.err = fmt.Errorf("configtable: unsupported type %q", bits[1])
 			return d.err
 		}
 
 		columns[n] = column{
 			name:    bits[0],
-			colType: bits[1],
+			colType: blizzType[0],
+			byteLen: byteLen,
 		}
 
 		if _, ok := columnNames[bits[0]]; ok {
@@ -81,6 +96,109 @@ func (d *Decoder) readHeader() error {
 	}
 	d.columns = columns
 	d.columnNames = columnNames
+
+	return nil
+}
+
+func byteWidth(k reflect.Kind) (width int, unsigned bool) {
+	switch k {
+	case reflect.Int, reflect.Uint:
+		return 4, k == reflect.Uint // Go spec specifies at least 32-bits in size
+	case reflect.Int8, reflect.Uint8:
+		return 1, k == reflect.Uint8
+	case reflect.Int16, reflect.Uint16:
+		return 2, k == reflect.Uint16
+	case reflect.Int32, reflect.Uint32:
+		return 4, k == reflect.Uint32
+	case reflect.Int64, reflect.Uint64:
+		return 8, k == reflect.Uint64
+	}
+	panic(fmt.Sprintf("cannot handle kind %v", k))
+}
+
+func isValidPairing(from column, to reflect.Type) bool {
+	k := to.Kind()
+	switch {
+	case k == reflect.String:
+		// can always convert into a string literally
+		return true
+
+	case from.colType == "string" && k == reflect.Slice && to.Elem().Kind() == reflect.String:
+		// can convert "string" into a slice of strings
+		return true
+
+	case from.colType == "dec":
+		// can convert dec into an integer of sufficient width
+		bw, _ := byteWidth(k)
+		return bw >= from.byteLen
+
+	case from.colType == "hex":
+		switch {
+		case k == reflect.Slice && to.Elem().Kind() == reflect.Uint8:
+			// can convert hex into a slice of bytes
+			return true
+		case k == reflect.Array && to.Elem().Kind() == reflect.Uint8:
+			// can convert hex into an array of bytes of exactly the correct length
+			return to.Len() == from.byteLen
+		}
+	}
+	return false
+}
+
+func convertTo(columnDelimiter *string, from column, value string, to reflect.Value) error {
+	k := to.Kind()
+	switch {
+	case k == reflect.String:
+		to.SetString(value)
+
+	case from.colType == "string" && k == reflect.Slice && to.Type().Elem().Kind() == reflect.String:
+		// can convert "string" into a slice of strings
+		delim := " "
+		if columnDelimiter != nil {
+			delim = *columnDelimiter
+		}
+		bits := strings.Split(value, delim)
+		bitsV := reflect.ValueOf(bits)
+		to.Set(bitsV)
+
+	case from.colType == "dec":
+		// can convert dec into an integer of sufficient width
+		bw, unsigned := byteWidth(k)
+		if unsigned {
+			v, err := strconv.ParseUint(value, 10, bw*8)
+			if err != nil {
+				return fmt.Errorf("parsing %q: %v", value, err)
+			}
+			to.SetUint(v)
+		} else {
+			v, err := strconv.ParseInt(value, 10, bw*8)
+			if err != nil {
+				return fmt.Errorf("parsing %q: %v", value, err)
+			}
+			to.SetInt(v)
+		}
+
+	case from.colType == "hex":
+		switch {
+		case k == reflect.Slice && to.Type().Elem().Kind() == reflect.Uint8:
+			v, err := hex.DecodeString(value)
+			if err != nil {
+				return fmt.Errorf("parsing %q: %v", value, err)
+			}
+			to.SetBytes(v)
+		case k == reflect.Array && to.Type().Elem().Kind() == reflect.Uint8:
+			// can convert hex into an array of bytes of exactly the correct length
+			vs, err := hex.DecodeString(value)
+			if err != nil {
+				return fmt.Errorf("parsing %q: %v", value, err)
+			}
+			arrLen := to.Len()
+			for n, v := range vs {
+				newN := arrLen - (len(vs) - n)
+				to.Index(newN).SetUint(uint64(v))
+			}
+		}
+	}
 
 	return nil
 }
@@ -128,13 +246,8 @@ func (d *Decoder) Decode(s interface{}) error {
 			continue
 		}
 
-		switch {
-		case f.Type.Kind() == reflect.String:
-			break
-		case f.Type.Kind() == reflect.Slice && f.Type.Elem().Kind() == reflect.String:
-			break
-		default:
-			return fmt.Errorf("configtable: can only decode string or string-slices")
+		if !isValidPairing(d.columns[columnID], f.Type) {
+			return fmt.Errorf("configtable: cannot decode %v into %v", d.columns[columnID], f.Type)
 		}
 
 		columnToField[columnID] = v.Field(n)
@@ -159,18 +272,15 @@ func (d *Decoder) Decode(s interface{}) error {
 		if !ok {
 			continue
 		}
-		vt := v.Type()
-		switch vt.Kind() {
-		case reflect.String:
-			columnToField[n].SetString(s)
-		case reflect.Slice:
-			delim := " "
-			if d, ok := columnDelimiters[n]; ok {
-				delim = d
-			}
-			bits := strings.Split(s, delim)
-			bitsV := reflect.ValueOf(bits)
-			columnToField[n].Set(bitsV)
+
+		var delim *string
+		if d, ok := columnDelimiters[n]; ok {
+			delim = &d
+		}
+
+		if err := convertTo(delim, d.columns[n], s, v); err != nil {
+			d.err = fmt.Errorf("configtable: %v", err)
+			return d.err
 		}
 	}
 
--- ngdp/configtable/configtable_test.go
+++ ngdp/configtable/configtable_test.go
@@ -13,6 +13,10 @@ blah|blah|blah
 foo|foo|foo foo2
 baa|bab|bac bad bae
 `
+
+	complexExampleTable = `Region!STRING:0|BuildConfig!HEX:16|CDNConfig!HEX:16|KeyRing!HEX:16|BuildId!DEC:4|VersionsName!String:0|ProductConfig!HEX:16|OtherNumber!DEC:4
+us|a423790b9bcee8ac532ceb39fe550685|c8043457fcf9eb6dac433e53fa47f5||44247|2.5.0.44247|f03448a5aa6c9f1e9307335946af05|27
+`
 )
 
 func TestLine(t *testing.T) {
@@ -47,9 +51,9 @@ func TestReadHeader(t *testing.T) {
 	}
 
 	want := []column{
-		{"Name", "STRING:0"},
-		{"Path", "STRING:0"},
-		{"Hosts", "STRING:0"},
+		{"Name", "string", 0},
+		{"Path", "string", 0},
+		{"Hosts", "string", 0},
 	}
 	got := d.columns
 	if !reflect.DeepEqual(got, want) {
@@ -74,8 +78,10 @@ func TestReadHeaderEOF(t *testing.T) {
 func TestReadHeaderErrors(t *testing.T) {
 	for _, test := range []string{
 		"NoType\n",
-		"UnsupportedType!INT:0\n",
+		"UnsupportedType!FOO:0\n",
 		"Duplicate!STRING:0|Duplicate!STRING:0\n",
+		"NoByteLength!FOO\n",
+		"BadByteLength!FOO:BAR\n",
 	} {
 		d := NewDecoder(strings.NewReader(test))
 		if err := d.readHeader(); err == nil {
@@ -151,4 +157,87 @@ func TestDecodeColumnCountMismatch(t *testing.T) {
 	if err := d.Decode(&s); err == nil {
 		t.Errorf("d.Decode: %v; want error", err)
 	}
+}
+
+func TestDecodeBadDecode(t *testing.T) {
+	for _, test := range []struct {
+		inp string
+		s   interface{}
+	}{
+		{"Test!DEC:1\nZZZ\n", &struct{ Test uint8 }{}},
+		{"Test!DEC:1\nZZZ\n", &struct{ Test int8 }{}},
+		{"Test!HEX:1\nZZZ\n", &struct{ Test []byte }{}},
+		{"Test!HEX:1\nZZZ\n", &struct{ Test [1]byte }{}},
+	} {
+		d := NewDecoder(strings.NewReader(test.inp))
+		if err := d.Decode(test.s); err == nil {
+			t.Errorf("d.Decode: %v; want error", err)
+		}
+	}
+}
+
+func TestDecodeComplexExample(t *testing.T) {
+	d := NewDecoder(strings.NewReader(complexExampleTable))
+	type Version struct {
+		Region        string
+		BuildConfig   string
+		CDNConfig     []byte
+		BuildId       int32
+		VersionsName  string
+		ProductConfig [16]byte
+		OtherNumber   uint32
+	}
+	want := Version{
+		"us",
+		"a423790b9bcee8ac532ceb39fe550685",
+		[]byte{0xc8, 0x04, 0x34, 0x57, 0xfc, 0xf9, 0xeb, 0x6d, 0xac, 0x43, 0x3e, 0x53, 0xfa, 0x47, 0xf5},
+		44247,
+		"2.5.0.44247",
+		[16]byte{0x00, 0xf0, 0x34, 0x48, 0xa5, 0xaa, 0x6c, 0x9f, 0x1e, 0x93, 0x07, 0x33, 0x59, 0x46, 0xaf, 0x05},
+		27,
+	}
+	var got Version
+	if err := d.Decode(&got); err != nil {
+		t.Errorf("d.Decode: %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got=%v; want=%v", got, want)
+	}
+}
+
+func TestByteWidth(t *testing.T) {
+	for _, test := range []struct {
+		s        interface{}
+		width    int
+		unsigned bool
+	}{
+		{struct{ T int }{}, 4, false},
+		{struct{ T uint }{}, 4, true},
+		{struct{ T int8 }{}, 1, false},
+		{struct{ T uint8 }{}, 1, true},
+		{struct{ T int16 }{}, 2, false},
+		{struct{ T uint16 }{}, 2, true},
+		{struct{ T int32 }{}, 4, false},
+		{struct{ T uint32 }{}, 4, true},
+		{struct{ T int64 }{}, 8, false},
+		{struct{ T uint64 }{}, 8, true},
+	} {
+		k := reflect.ValueOf(test.s).Type().Field(0).Type.Kind()
+		width, unsigned := byteWidth(k)
+		if width != test.width {
+			t.Errorf("%v: byteWidth width=%v; want %v", k, width, test.width)
+		}
+		if unsigned != test.unsigned {
+			t.Errorf("%v: byteWidth unsigned=%v; want %v", k, unsigned, test.unsigned)
+		}
+	}
+}
+
+func TestByteWidthPanic(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("expected panic, got nothing")
+		}
+	}()
+	byteWidth(reflect.ValueOf(nil).Kind())
 }
