9300420b1c0f9d07f0b8ea8f416df73f13e65307
Luke Granger-Brown <git@lukegb.com>
2017-04-02 20:37:41 +0100

    ngdp: create consts and types

---
+++ ngdp/consts.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2017 Luke Granger-Brown
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ngdp
+
+type ProgramCode string
+
+const (
+	// TODO(lukegb): include the rest once tested
+	ProgramHotS ProgramCode = "hero"
+)
+
+type Region string
+
+const (
+	RegionUnitedStates Region = "us"
+	RegionEurope       Region = "eu"
+	RegionChina        Region = "cn"
+	RegionKorea        Region = "kr"
+	RegionTaiwan       Region = "tw"
+	RegionSingapore    Region = "sg"
+)
+
+var DefaultRegion = RegionEurope
+
+type ContentType string
+
+const (
+	ContentTypeConfig ContentType = "config"
+	ContentTypeData   ContentType = "data"
+	ContentTypePatch  ContentType = "patch"
+)
