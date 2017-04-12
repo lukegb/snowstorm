bc2299a56164fa45fb58e7f4671c3c054750b384
Luke Granger-Brown <git@lukegb.com>
2017-04-12 18:50:08 +0100

    client: move more things into low level client

--- ngdp/client/highlevel.go
+++ ngdp/client/highlevel.go
@@ -24,12 +24,10 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
-	"golang.org/x/sync/errgroup"
 
 	"github.com/lukegb/snowstorm/blte"
 	"github.com/lukegb/snowstorm/ngdp"
 	"github.com/lukegb/snowstorm/ngdp/encoding"
-	"github.com/lukegb/snowstorm/ngdp/keyvalue"
 )
 
 var (
@@ -74,130 +72,25 @@ type Client struct {
 // New creates a new Client for the given ProgramCode and Region.
 //
 // It will automatically create an ArchiveMapper and Encoder as appropriate.
-func New(octx context.Context, program ngdp.ProgramCode, region ngdp.Region) (*Client, error) {
+func New(ctx context.Context, program ngdp.ProgramCode, region ngdp.Region) (*Client, error) {
 	glog.Info("Initialising new NGDP Client")
 	llc := &LowLevelClient{}
 
 	// Fetch CDN and Version info.
-	var cdn ngdp.CDNInfo
-	var version ngdp.VersionInfo
-	g, ctx := errgroup.WithContext(octx)
-	g.Go(func() error {
-		glog.Info("Retrieving CDN info")
-		cdns, err := llc.cdns(ctx, program, region)
-		if err != nil {
-			return errors.Wrap(err, "retrieving CDN info")
-		}
-		for _, c := range cdns {
-			if c.Name == region {
-				cdn = c
-				return nil
-			}
-		}
-		return ErrUnknownRegion
-	})
-	g.Go(func() error {
-		glog.Info("Retrieving version info")
-		versions, err := llc.versions(ctx, program, region)
-		if err != nil {
-			return errors.Wrap(err, "retrieving version info")
-		}
-		for _, v := range versions {
-			if v.Region == region {
-				version = v
-				return nil
-			}
-		}
-		return ErrUnknownRegion
-	})
-	if err := g.Wait(); err != nil {
+	cdn, version, err := llc.Info(ctx, program, region)
+	if err != nil {
 		return nil, err
 	}
 
 	// Fetch Build and CDN configs.
-	var buildConfig ngdp.BuildConfig
-	var cdnConfig ngdp.CDNConfig
-	g, ctx = errgroup.WithContext(octx)
-	g.Go(func() error {
-		glog.Info("Retrieving build config")
-		resp, err := llc.get(ctx, cdn, ngdp.ContentTypeConfig, version.BuildConfig, "")
-		if err != nil {
-			return errors.Wrap(err, "retrieving build config")
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
-		}
-
-		glog.Info("Parsing build config")
-		if err := keyvalue.Decode(resp.Body, &buildConfig); err != nil {
-			return errors.Wrap(err, "parsing build config")
-		}
-
-		return nil
-	})
-	g.Go(func() error {
-		glog.Info("Retrieving CDN config")
-		resp, err := llc.get(ctx, cdn, ngdp.ContentTypeConfig, version.CDNConfig, "")
-		if err != nil {
-			return errors.Wrap(err, "retrieving cdn config")
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
-		}
-
-		glog.Info("Parsing CDN config")
-		if err := keyvalue.Decode(resp.Body, &cdnConfig); err != nil {
-			return errors.Wrap(err, "parsing cdn config")
-		}
-
-		return nil
-	})
-	if err := g.Wait(); err != nil {
+	cdnConfig, buildConfig, err := llc.Configs(ctx, cdn, version)
+	if err != nil {
 		return nil, err
 	}
 
 	// Build encoding and archive mappers.
-	g, ctx = errgroup.WithContext(octx)
-	var archiveMapper *ArchiveMapper
-	var encodingMapper *encoding.Mapper
-	g.Go(func() error {
-		glog.Info("Downloading encoding table")
-		resp, err := llc.get(ctx, cdn, ngdp.ContentTypeData, buildConfig.Encoding.CDNHash, "")
-		if err != nil {
-			return errors.Wrap(err, "downloading encoding table")
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
-		}
-
-		glog.Info("Parsing encoding table")
-		mapper, err := encoding.NewMapper(blte.NewReader(resp.Body))
-		if err != nil {
-			return errors.Wrap(err, "parsing encoding table")
-		}
-		glog.Info("Done building encoding mapper")
-
-		encodingMapper = mapper
-		return nil
-	})
-	g.Go(func() error {
-		glog.Info("Building archive mapper")
-		am, err := llc.NewArchiveMapper(ctx, cdn, cdnConfig.Archives)
-		if err != nil {
-			return errors.Wrap(err, "building archive mapper")
-		}
-		glog.Info("Done building archive mapper")
-
-		archiveMapper = am
-		return nil
-	})
-	if err := g.Wait(); err != nil {
+	encodingMapper, archiveMapper, err := llc.Mappers(ctx, cdn, cdnConfig, buildConfig)
+	if err != nil {
 		return nil, err
 	}
 
--- ngdp/client/lowlevel.go
+++ ngdp/client/lowlevel.go
@@ -22,8 +22,15 @@ import (
 	"io"
 	"net/http"
 
+	"golang.org/x/sync/errgroup"
+
+	"github.com/golang/glog"
+	"github.com/lukegb/snowstorm/blte"
 	"github.com/lukegb/snowstorm/ngdp"
 	"github.com/lukegb/snowstorm/ngdp/configtable"
+	"github.com/lukegb/snowstorm/ngdp/encoding"
+	"github.com/lukegb/snowstorm/ngdp/keyvalue"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -34,6 +41,21 @@ var (
 // A LowLevelClient provides simple wrappers to make basic NGDP operations easier.
 type LowLevelClient struct {
 	Client *http.Client
+}
+
+// Fetch retrieves a piece of data content by its CDNHash.
+func (c *LowLevelClient) Fetch(ctx context.Context, cdnInfo ngdp.CDNInfo, cdnHash ngdp.CDNHash) (io.ReadCloser, error) {
+	resp, err := c.get(ctx, cdnInfo, ngdp.ContentTypeData, cdnHash, "")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
+	}
+
+	r := blte.NewReader(resp.Body)
+	return newWrappedCloser(r, resp.Body), nil
 }
 
 func (c *LowLevelClient) get(ctx context.Context, cdnInfo ngdp.CDNInfo, contentType ngdp.ContentType, cdnHash ngdp.CDNHash, suffix string) (*http.Response, error) {
@@ -123,4 +145,164 @@ func cdnURL(cdnInfo ngdp.CDNInfo, contentType ngdp.ContentType, cdnHash ngdp.CDN
 
 func patchURL(program ngdp.ProgramCode, region ngdp.Region, suffix string) string {
 	return fmt.Sprintf("http://%s.patch.battle.net:1119/%s/%s", region, program, suffix)
+}
+
+func (c *LowLevelClient) CDN(ctx context.Context, program ngdp.ProgramCode, region ngdp.Region) (ngdp.CDNInfo, error) {
+	cdns, err := c.cdns(ctx, program, region)
+	if err != nil {
+		return ngdp.CDNInfo{}, errors.Wrap(err, "retrieving CDN info")
+	}
+
+	for _, c := range cdns {
+		if c.Name == region {
+			return c, nil
+		}
+	}
+
+	return ngdp.CDNInfo{}, ErrUnknownRegion
+}
+
+func (c *LowLevelClient) Version(ctx context.Context, program ngdp.ProgramCode, region ngdp.Region) (ngdp.VersionInfo, error) {
+	versions, err := c.versions(ctx, program, region)
+	if err != nil {
+		return ngdp.VersionInfo{}, errors.Wrap(err, "retrieving version info")
+	}
+
+	for _, c := range versions {
+		if c.Region == region {
+			return c, nil
+		}
+	}
+
+	return ngdp.VersionInfo{}, ErrUnknownRegion
+}
+
+func (c *LowLevelClient) BuildConfig(ctx context.Context, cdn ngdp.CDNInfo, version ngdp.VersionInfo) (ngdp.BuildConfig, error) {
+	resp, err := c.get(ctx, cdn, ngdp.ContentTypeConfig, version.BuildConfig, "")
+	if err != nil {
+		return ngdp.BuildConfig{}, errors.Wrap(err, "retrieving build config")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return ngdp.BuildConfig{}, errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
+	}
+
+	var buildConfig ngdp.BuildConfig
+	if err := keyvalue.Decode(resp.Body, &buildConfig); err != nil {
+		return ngdp.BuildConfig{}, errors.Wrap(err, "parsing build config")
+	}
+
+	return buildConfig, nil
+}
+
+func (c *LowLevelClient) CDNConfig(ctx context.Context, cdn ngdp.CDNInfo, version ngdp.VersionInfo) (ngdp.CDNConfig, error) {
+	resp, err := c.get(ctx, cdn, ngdp.ContentTypeConfig, version.CDNConfig, "")
+	if err != nil {
+		return ngdp.CDNConfig{}, errors.Wrap(err, "retrieving cdn config")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return ngdp.CDNConfig{}, errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
+	}
+
+	var cdnConfig ngdp.CDNConfig
+	if err := keyvalue.Decode(resp.Body, &cdnConfig); err != nil {
+		return ngdp.CDNConfig{}, errors.Wrap(err, "parsing cdn config")
+	}
+
+	return cdnConfig, nil
+}
+
+func (c *LowLevelClient) EncodingTable(ctx context.Context, cdn ngdp.CDNInfo, encodingHash ngdp.CDNHash) (*encoding.Mapper, error) {
+	resp, err := c.get(ctx, cdn, ngdp.ContentTypeData, encodingHash, "")
+	if err != nil {
+		return nil, errors.Wrap(err, "downloading encoding table")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errBadStatus{resp.StatusCode, resp.Status, http.StatusOK}
+	}
+
+	mapper, err := encoding.NewMapper(blte.NewReader(resp.Body))
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing encoding table")
+	}
+	return mapper, nil
+}
+
+func (c *LowLevelClient) ArchiveMapper(ctx context.Context, cdn ngdp.CDNInfo, archives []ngdp.CDNHash) (*ArchiveMapper, error) {
+	am, err := c.NewArchiveMapper(ctx, cdn, archives)
+	if err != nil {
+		return nil, errors.Wrap(err, "building archive mapper")
+	}
+	return am, nil
+}
+
+func (c *LowLevelClient) Info(ctx context.Context, program ngdp.ProgramCode, region ngdp.Region) (ngdp.CDNInfo, ngdp.VersionInfo, error) {
+	var cdn ngdp.CDNInfo
+	var version ngdp.VersionInfo
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		glog.Info("Retrieving CDN info")
+		var err error
+		cdn, err = c.CDN(ctx, program, region)
+		return err
+	})
+	g.Go(func() error {
+		glog.Info("Retrieving version info")
+		var err error
+		version, err = c.Version(ctx, program, region)
+		return err
+	})
+	if err := g.Wait(); err != nil {
+		return ngdp.CDNInfo{}, ngdp.VersionInfo{}, err
+	}
+	return cdn, version, nil
+}
+
+func (c *LowLevelClient) Configs(ctx context.Context, cdn ngdp.CDNInfo, version ngdp.VersionInfo) (ngdp.CDNConfig, ngdp.BuildConfig, error) {
+	var cdnConfig ngdp.CDNConfig
+	var buildConfig ngdp.BuildConfig
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		glog.Info("Retrieving build config")
+		var err error
+		buildConfig, err = c.BuildConfig(ctx, cdn, version)
+		return err
+	})
+	g.Go(func() error {
+		glog.Info("Retrieving CDN config")
+		var err error
+		cdnConfig, err = c.CDNConfig(ctx, cdn, version)
+		return err
+	})
+	if err := g.Wait(); err != nil {
+		return ngdp.CDNConfig{}, ngdp.BuildConfig{}, err
+	}
+	return cdnConfig, buildConfig, nil
+}
+
+func (c *LowLevelClient) Mappers(ctx context.Context, cdn ngdp.CDNInfo, cdnConfig ngdp.CDNConfig, buildConfig ngdp.BuildConfig) (*encoding.Mapper, *ArchiveMapper, error) {
+	var encodingMapper *encoding.Mapper
+	var archiveMapper *ArchiveMapper
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		glog.Info("Downloading encoding table")
+		var err error
+		encodingMapper, err = c.EncodingTable(ctx, cdn, buildConfig.Encoding.CDNHash)
+		return err
+	})
+	g.Go(func() error {
+		glog.Info("Building archive mapper")
+		var err error
+		archiveMapper, err = c.ArchiveMapper(ctx, cdn, cdnConfig.Archives)
+		return err
+	})
+	if err := g.Wait(); err != nil {
+		return nil, nil, err
+	}
+	return encodingMapper, archiveMapper, nil
 }
